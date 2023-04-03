91e3087d02c71be757ff4409ab08f0cc58e9e959
Raissa <raissa.frota@capgemini.com>
2023-04-03 14:34:42 -0300

    Criando o projeto de JSON Consumer

---
+++ json-consumer/src/main/java/br/com/raissafrota/jsonconsumer/model/Payment.java
@@ -0,0 +1,15 @@
+package br.com.raissafrota.jsonconsumer.model;
+
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+public class Payment implements Serializable {
+
+    private Long id;
+    private Long idUser;
+    private Long idProduct;
+    private String cardNumber;
+}
